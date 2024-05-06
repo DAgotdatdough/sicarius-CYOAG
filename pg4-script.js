dff7cf638006f42a23a4728b28f2263b6807a271
DAgotdatdough <40508136@live.napier.ac.uk>
2024-05-06 02:10:57 +0100

    changes

--- pg4-script.js
+++ pg4-script.js
@@ -48,6 +48,10 @@ document.addEventListener('DOMContentLoaded', function() {
     function endGame() {
         attackButton.disabled = true;
         gameMessage.textContent = (assassinHealth > 0) ? "Assassin wins!" : "Guard wins!";
+        document.getElementById('next-page-button').style.display = 'block';
+        document.getElementById('next-page-button').addEventListener('click', function() {
+            window.location.href = 'pg5.html'; 
+        });
     }
 
     function triggerAnimations() {
@@ -70,5 +74,33 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
+function toggleMute() {
+    const audio = document.querySelector("audio");
+    const isMuted = localStorage.getItem('muted') === 'true';
+    localStorage.setItem('muted', !isMuted);
+    document.getElementById('mute_img').src = isMuted ? 'images/music-on.png' : 'images/music-off.png';
+    if (isMuted) {
+        audio.volume = 0.2;
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+
+
+
+function loadMuteImg() {
+    const isMuted = localStorage.getItem('muted') === 'true';
+    document.getElementById('mute_img').src = isMuted ? 'images/music-off.png' : 'images/music-on.png';
+    if (isMuted) {
+        document.querySelector("audio").pause();
+    } else {
+        document.querySelector("audio").play();
+    }
+}
+
+window.onload = loadMuteImg;
 
 
--- script.js
+++ script.js
@@ -9,12 +9,33 @@ document.getElementById('help-btn').addEventListener('click', function() {
 });
 
 
+function toggleMute() {
+    const audio = document.querySelector("audio");
+    const isMuted = localStorage.getItem('muted') === 'true';
+    localStorage.setItem('muted', !isMuted);
+    document.getElementById('mute_img').src = isMuted ? 'images/music-on.png' : 'images/music-off.png';
+    if (isMuted) {
+        audio.volume = 0.2;
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
 
 
 
 
+function loadMuteImg() {
+    const isMuted = localStorage.getItem('muted') === 'true';
+    document.getElementById('mute_img').src = isMuted ? 'images/music-off.png' : 'images/music-on.png';
+    if (isMuted) {
+        document.querySelector("audio").pause();
+    } else {
+        document.querySelector("audio").play();
+    }
+}
 
-
+window.onload = loadMuteImg;
 
 
 
